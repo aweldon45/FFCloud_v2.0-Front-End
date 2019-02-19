54cb0b79f24a899f6035d515df5b76430c5d38d3
aweldon45 <aweldon@LAPTOP-E81G01BQ.localdomain>
2019-02-19 06:26:20 -0500

    update serverjs

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res){
-  res.sendfile(__dirname + "/dist/index.html");
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
 app.listen(port);
